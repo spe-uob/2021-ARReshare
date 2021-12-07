b6fe89fe73f1837eaf810ea07909a6b0fd7e532c
Artur Varosyan <artur.varosyan1@gmail.com>
2021-12-07 21:10:13 +0000

    Change MapView to use ExampleData

--- app/src/main/java/com/example/ar_reshare/MapsActivity.java
+++ app/src/main/java/com/example/ar_reshare/MapsActivity.java
@@ -82,7 +82,7 @@ public class MapsActivity extends FragmentActivity implements
         mMap.setOnInfoWindowClickListener(this);
 
         // Create dummy products and display them on the map
-        List<Product> products = createDummyProducts();
+        List<Product> products = ExampleData.getProducts();
         populateMap(mMap, products);
 
         // Default map starting location
@@ -193,29 +193,6 @@ public class MapsActivity extends FragmentActivity implements
         startActivity(intent);
     }
 
-
-    /* ---------------------------------------------
-            TEMPORARY CLASS AND HARDCODED DATA
-       --------------------------------------------- */
-
-    // Creates a list of dummy products for testing and development
-    private List<Product> createDummyProducts () {
-        List<Product> products = new ArrayList<>();
-
-        User user0 = new User("John","",1);
-        products.add(new Product("Fancy Cup", "This is a really nice fancy cup. Feel free to message me to arrange a pickup.", user0, Category.OTHER,51.45120306024447, -2.5869936269149303));
-        User user1 = new User("Artur","",1);
-        products.add(new Product("Java for Beginners Book", "This is a book extremely helpful for your scotland yard coursework. Feel free to message me to arrange a pickup.", user1, Category.BOOKS, 51.45599668866024, -2.6030781306216135));
-        User user2 = new User("Lingtao","",1);
-        products.add(new Product("Pink Umbrella", "This is a very pink umb.", user2, Category.CLOTHING, 51.45416805430673, -2.591828561043675));
-        User user3 = new User("Hellin","",1);
-        products.add(new Product("Apple Pencil", "This is a product.", user3, Category.ELECTRONICS,51.45864853294286, -2.5853638594577193));
-        User user4 = new User("Ziqian","",1);
-        products.add(new Product("Meat", "This is a product.", user4, Category.FOOD, 51.45692540090406, -2.6081114869801714));
-        User user5 = new User("Arafat","",1);
-        products.add(new Product("Pink Headphones", "This is a product.", user5, Category.ELECTRONICS, 51.459040571152514, -2.6022736036387366));
-        return products;
-    }
 
     // Populates the map with markers given a list of products
     private void populateMap(GoogleMap mMap, List<Product> products) {
