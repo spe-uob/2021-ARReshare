c24aad8b24a2d6c3e76a05e5a6cc6a4b6227495d
Manoutofpower <ysfeihao@163.com>
2021-12-08 18:07:53 +0000

    Coneected with everybody, change the page view when there is no pictures.

--- app/src/main/java/com/example/ar_reshare/ProfileActivity.java
+++ app/src/main/java/com/example/ar_reshare/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.ar_reshare;
 
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,21 +8,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
     private Button btM;
     private Button btS;
     private ImageButton btB;
+    private ImageButton btImto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        User S = new User("Godblessme", "12345", 1);
+
+        User A = ExampleData.getUsers().get(1);
+        Product B = ExampleData.getProducts().get(1);
+
         TextView name = findViewById(R.id.username);
-        name.setText(S.getName());
+        name.setText(A.getName());
+
+        TextView bio = findViewById(R.id.description);
+        bio.setText(A.getBio());
+
+        ImageView avator1 = findViewById(R.id.avator);
+        avator1.setImageResource(A.getProfileIcon());
+
+        ImageButton product1 = findViewById(R.id.shared1);
+        product1.setImageResource(B.getImages().get(0));
 
         btM = (Button)findViewById(R.id.btM);
         btM.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +54,21 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btImto = (ImageButton) findViewById(R.id.shared1);
+        btImto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, ProductPageActivity.class);
+                intent.putExtra("name", A.getName());
+                intent.putExtra("product", B);
+                intent.putExtra("contributor", B.getContributor());
+                intent.putExtra("profilePicId", B.getImages().get(0));
+                intent.putIntegerArrayListExtra("productPicId", (ArrayList<Integer>) B.getImages());
+
                 startActivity(intent);
             }
         });
