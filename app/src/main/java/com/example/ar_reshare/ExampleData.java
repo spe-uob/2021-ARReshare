d613aa62c8702105693b9ac393100a0715020c16
arfi2212 <uj20898@bristol.ac.uk>
2021-12-09 14:24:21 +0000

    changed descriptions

--- app/src/main/java/com/example/ar_reshare/ExampleData.java
+++ app/src/main/java/com/example/ar_reshare/ExampleData.java
@@ -23,12 +23,12 @@ public class ExampleData {
         User user1 = new User("Artur","",1);
         user1.setProfileIcon(R.drawable.artur_profile_icon);
         users.add(user1);
-        products.add(new Product("Festive Coffee Cup", "Get your hands on this beautiful coffee cup now!", user1, Category.OTHER, 51.456070226943865, -2.602992299931959));
+        products.add(new Product("Festive Coffee Cup", "Get your hands on this beautiful coffee cup :)", user1, Category.OTHER, 51.456070226943865, -2.602992299931959));
         products.get(1).addImages(R.drawable.coffee_cup);
 
         User user2 = new User("Lingtao","",1);
         users.add(user2);
-        products.add(new Product("Pink Umbrella", "This is a very pink umb.", user2, Category.CLOTHING, 51.45416805430673, -2.591828561043675));
+        products.add(new Product("Pink Umbrella", "Who wouldn't this fashionable pink umbrella?", user2, Category.CLOTHING, 51.45416805430673, -2.591828561043675));
         user2.setProfileIcon(R.drawable.lingtao_profile_icon);
         products.get(2).addImages(R.drawable.umbrella1);
         products.get(2).addImages(R.drawable.umbrella2);
@@ -36,14 +36,14 @@ public class ExampleData {
 
         User user3 = new User("Hellin","",1);
         users.add(user3);
-        products.add(new Product("Apple Pencil", "This is a product.", user3, Category.ELECTRONICS,51.45864853294286, -2.5853638594577193));
+        products.add(new Product("Apple Pencil", "Try out this Apple pencil on your new iPad now!", user3, Category.ELECTRONICS,51.45864853294286, -2.5853638594577193));
         user3.setProfileIcon(R.drawable.user);
         products.get(3).addImages(R.drawable.applepencil1);
         products.get(3).addImages(R.drawable.applepencil2);
 
         User user4 = new User("Ziqian","",1);
         users.add(user4);
-        products.add(new Product("Meat", "This is a product.", user4, Category.FOOD, 51.45692540090406, -2.6081114869801714));
+        products.add(new Product("Meat", "Leftovers from my lit bbq party last night!", user4, Category.FOOD, 51.45692540090406, -2.6081114869801714));
         user4.setProfileIcon(R.drawable.ziqian);
         products.get(4).addImages(R.drawable.meal1);
         products.get(4).addImages(R.drawable.meal2);
