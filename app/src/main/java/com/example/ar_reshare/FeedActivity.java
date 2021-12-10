fb035e353c723f418b3a53b911bee76fd0684a85
arfi2212 <uj20898@bristol.ac.uk>
2021-12-10 13:27:05 +0000

    testing:

--- app/src/main/java/com/example/ar_reshare/FeedActivity.java
+++ app/src/main/java/com/example/ar_reshare/FeedActivity.java
@@ -27,7 +27,7 @@ public class FeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-        // This needs to be done temporarily since not all users have icons, and not all products have images
+        // This is done temporarily since not all users have icons, and not all products have images
         arrayList.add(productsList.get(1));
         arrayList.add(productsList.get(2));
         arrayList.add(productsList.get(3));
