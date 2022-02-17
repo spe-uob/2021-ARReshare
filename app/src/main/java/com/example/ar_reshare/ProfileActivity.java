f6c3458658e5bc06aa3057f4d64e0e904875f755
Manoutofpower <ysfeihao@163.com>
2022-02-17 15:26:48 +0000

    Fix bug on profile page. Make profile page dynamic.

--- app/src/main/java/com/example/ar_reshare/ProfileActivity.java
+++ app/src/main/java/com/example/ar_reshare/ProfileActivity.java
@@ -11,31 +11,40 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
     private Button MessageButton;
     private ImageButton BackButton;
     private ImageButton ProductButton;
+    private Product currentProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Intent i= getIntent();
+        User currentUser = i.getParcelableExtra("user");
 
-        User A = ExampleData.getUsers().get(0);
-        Product B = ExampleData.getProducts().get(0);
+        User A = currentUser;
+        List<Product> products = ExampleData.getProducts();
 
         TextView name = findViewById(R.id.username);
-        name.setText(A.getName());
+        name.setText(currentUser.getName());
 
         TextView bio = findViewById(R.id.description);
-        bio.setText(A.getBio());
+        bio.setText(currentUser.getBio());
 
         ImageView avator1 = findViewById(R.id.avator);
-        avator1.setImageResource(A.getProfileIcon());
+        avator1.setImageResource(currentUser.getProfileIcon());
 
         ImageButton product1 = findViewById(R.id.shared1);
-        product1.setImageResource(B.getImages().get(0));
+        for (Product product : products) {
+            if(product.getContributor().equals(currentUser)){
+                currentProduct = product;
+            }
+        }
+        product1.setImageResource(currentProduct.getImages().get(0));
 
         MessageButton = (Button)findViewById(R.id.btM);
         MessageButton.setOnClickListener(new View.OnClickListener() {
@@ -60,10 +69,10 @@ public class ProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(ProfileActivity.this, ProductPageActivity.class);
                 intent.putExtra("name", A.getName());
-                intent.putExtra("product", B);
-                intent.putExtra("contributor", B.getContributor());
-                intent.putExtra("profilePicId", B.getContributor().getProfileIcon());
-                intent.putIntegerArrayListExtra("productPicId", (ArrayList<Integer>) B.getImages());
+                intent.putExtra("product", currentProduct);
+                intent.putExtra("contributor", currentProduct.getContributor());
+                intent.putExtra("profilePicId", currentProduct.getContributor().getProfileIcon());
+                intent.putIntegerArrayListExtra("productPicId", (ArrayList<Integer>) currentProduct.getImages());
 
                 startActivity(intent);
             }
