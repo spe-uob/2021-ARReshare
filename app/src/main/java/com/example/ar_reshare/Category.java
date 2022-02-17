54c6c4ff412c14104cfbd1711e27bba3a119d58d
Hellin Sun <hellinsunwork@gmail.com>
2022-02-17 17:15:15 +0000

    added obj models and added modelLocation attribute to Category

--- app/src/main/java/com/example/ar_reshare/Category.java
+++ app/src/main/java/com/example/ar_reshare/Category.java
@@ -1,17 +1,19 @@
 package com.example.ar_reshare;
 
 public enum Category {
-    CLOTHING(0),        // RED
-    SHOES(300),         // PINK
-    ELECTRONICS(180),   // CYAN BLUE
-    BOOKS(240),         // NAVY
-    FOOD(120),          // GREEN
-    OTHER(35);          // ORANGE
+    CLOTHING(0,"models/pawn.obj"),        // RED
+    SHOES(300,"models/pawn.obj"),         // PINK
+    ELECTRONICS(180,"models/pawn.obj"),   // CYAN BLUE
+    BOOKS(240,"models/pawn.obj"),         // NAVY
+    FOOD(120,"models/pawn.obj"),          // GREEN
+    OTHER(35,"models/pawn.obj");          // ORANGE
 
     private float hueColour;
+    private String modelLocation;
 
-    private Category(float colour) {
+    private Category(float colour, String modelLocation) {
         this.hueColour = colour;
+        this.modelLocation = modelLocation;
     }
 
     public float getCategoryColour() {
