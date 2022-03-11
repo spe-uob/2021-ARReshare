33a57efdc1bf400488a3b28e9d716260883acd63
arfi2212 <uj20898@bristol.ac.uk>
2022-03-11 16:44:51 +0000

    Replaced coloured background text with coloured icons representing categories

--- app/src/main/java/com/example/ar_reshare/Category.java
+++ app/src/main/java/com/example/ar_reshare/Category.java
@@ -3,24 +3,35 @@ package com.example.ar_reshare;
 import android.graphics.Color;
 
 public enum Category {
-    CLOTHING(Color.YELLOW,"models/hat.obj","models/purple.png"),        // YELLOW
-    ACCESSORIES(Color.MAGENTA, "models/pawn.obj","models/pink.png"), // PINK
-    ELECTRONICS(Color.CYAN,"models/phone.obj","models/grey.png"),   // CYAN
-    BOOKS(Color.RED,"models/pawn.obj","models/pink.png"),         // RED
-    FOOD(Color.GREEN,"models/burger.obj","models/burger.png"),          // GREEN
-    OTHER(Color.rgb(255, 165, 0),"models/cup.obj","models/pink.png");          // ORANGE
+    CLOTHING(Color.rgb(255, 165, 0),"models/hat.obj","models/purple.png",
+            R.drawable.ic_baseline_clothing_24),        // ORANGE
+    ACCESSORIES(Color.MAGENTA, "models/pawn.obj","models/pink.png",
+            R.drawable.ic_baseline_watch_24), // PINK
+    ELECTRONICS(Color.CYAN,"models/phone.obj","models/grey.png",
+            R.drawable.ic_baseline_computer_24),   // CYAN
+    BOOKS(Color.RED,"models/pawn.obj","models/pink.png",
+            R.drawable.ic_baseline_menu_book_24),         // RED
+    FOOD(Color.GREEN,"models/burger.obj","models/burger.png",
+            R.drawable.ic_baseline_fastfood_24),          // GREEN
+    OTHER(Color.GRAY,"models/cup.obj","models/pink.png",
+            R.drawable.ic_baseline_handyman_24);          // GRAY
 
     private final int hueColour;
     private String modelLocation;
     private String colorLocation;
+    private int categoryIcon;
 
-    Category(int colour, String modelLocation, String colorLocation) {
+    Category(int colour, String modelLocation, String colorLocation, int categoryIcon) {
         this.hueColour = colour;
         this.modelLocation = modelLocation;
         this.colorLocation = colorLocation;
+        this.categoryIcon = categoryIcon;
     }
 
     public int getCategoryColour() {
         return this.hueColour;
+    }
+    public int getCategoryIcon() {
+        return this.categoryIcon;
     }
 }
