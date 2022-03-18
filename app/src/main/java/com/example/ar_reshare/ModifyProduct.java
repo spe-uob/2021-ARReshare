18d6384e198331610ccacadc6941087068aa9a1d
Hellin Sun <hellinsunwork@gmail.com>
2022-03-18 22:13:47 +0000

    added product condition and category to product page / changed ui design

---
+++ app/src/main/java/com/example/ar_reshare/ModifyProduct.java
@@ -0,0 +1,14 @@
+package com.example.ar_reshare;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ModifyProduct extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_product);
+    }
+}
