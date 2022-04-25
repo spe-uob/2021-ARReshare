bf394a8b6e44f3291bc94c62dbb235892f54ab0f
Lingtao-Cai <78472848+Lingtao-Cai@users.noreply.github.com>
2022-04-25 18:34:10 +0100

    Add chats feature holding to delete to develop (#171)
    * fix bugs with data converter in messaging
    
    * fix bugs with multiChat page, set the default profile icon
    
    * implement holding to delete in multiChats page
    
    * fix the activity name in splash

--- app/src/main/java/com/example/ar_reshare/MessagingActivity.java
+++ app/src/main/java/com/example/ar_reshare/MessagingActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
@@ -150,24 +151,24 @@ public class MessagingActivity extends AppCompatActivity{
                 if (success) {
                     System.out.println("get conversations successful");
                     System.out.println(message);
-                    messageListAdapter.setMessageResult(messageResult, loggedInUserID);
-                    mMessageList.clear();
-                    int resSize = messageResult.getMessages().size();
-                    int mSize = mMessageList.size();
-                    if (resSize > mSize){
-                        int offset = resSize-mSize;
-                        List<Message> messages = new ArrayList<>();
-                        for (int i = resSize - 1;i>=(resSize-offset);i--){
-                            messageResult.getMessages().get(i).setProfileIcon(image);
-                            mMessageList.add(messageResult.getMessages().get(i));
-                            messageListAdapter.notifyDataSetChanged();
-                        }
-                    }
-                    recyclerView.setAdapter(messageListAdapter);
                 }else {
                     System.out.println(message);
                     System.out.println("fail to get conversations");
                 }
+                messageListAdapter.setMessageResult(messageResult, loggedInUserID);
+                mMessageList.clear();
+                int resSize = messageResult.getMessages().size();
+                int mSize = mMessageList.size();
+                if (resSize > mSize){
+                    int offset = resSize-mSize;
+                    List<Message> messages = new ArrayList<>();
+                    for (int i = resSize - 1;i>=(resSize-offset);i--){
+                        messageResult.getMessages().get(i).setProfileIcon(image);
+                        mMessageList.add(messageResult.getMessages().get(i));
+                        messageListAdapter.notifyDataSetChanged();
+                    }
+                }
+                recyclerView.setAdapter(messageListAdapter);
             }
         });
     }
@@ -193,7 +194,19 @@ public class MessagingActivity extends AppCompatActivity{
                     handler.post(refresh);
                 }else {
                     System.out.println("fail to get message profile icon image");
+                    getConversationByID(conversationId, BitmapFactory.decodeResource(null, R.mipmap.ic_launcher_round));
+                    refresh = new Runnable() {
+                        public void run() {
+                            // Do something
+                            System.out.println("refresh");
+                            getConversationByID(conversationId, BitmapFactory.decodeResource(null, R.mipmap.ic_launcher_round));
+                            messageListAdapter.notifyDataSetChanged();
+                            handler.postDelayed(refresh,500);
+                        }
+                    };
+                    handler.post(refresh);
                 }
+
             }
         });
     }
@@ -221,7 +234,7 @@ public class MessagingActivity extends AppCompatActivity{
     }
 
 
-//    convert to date as follows
+    //    convert to date as follows
 //    dates[0] day name(Mon, Tue, etc)
 //    dates[1] Month name
 //    dates[2] day number
@@ -243,4 +256,3 @@ public class MessagingActivity extends AppCompatActivity{
 
 
 }
-
